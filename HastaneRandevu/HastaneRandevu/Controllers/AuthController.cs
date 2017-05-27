fb5e85b4795006c5c02eb258307aa7deae5ec06c
ERAY\Eray <erydmr@gmail.com>
2017-05-27 13:31:27 +0300

    First

---
+++ HastaneRandevu/HastaneRandevu/Controllers/AuthController.cs
@@ -0,0 +1,33 @@
+﻿using HastaneRandevu.Models;
+using NHibernate.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using static HastaneRandevu.ViewModels.Auth;
+
+namespace HastaneRandevu.Controllers
+{
+    public class AuthController : Controller
+    {
+        // GET: Auth
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(AuthLogin form)
+        {
+            var post = Database.Session.Load<Hastalar>(1);
+            if (post == null)
+                return HttpNotFound();
+
+            Database.Session.Delete(post);
+
+            return RedirectToAction("home");
+        }
+    }
+}
---
+++ HastaneRandevu/HastaneRandevu/ViewModels/Auth.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HastaneRandevu.ViewModels
+{
+    public class Auth
+    {
+        public class AuthLogin
+        {
+            [Required]
+            public string TcKimlik { get; set; }
+
+            [Required]
+            [DataType(DataType.Password)]
+            public string Sifre { get; set; }
+
+        }
+    }
+}
